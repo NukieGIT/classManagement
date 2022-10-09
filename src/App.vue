d8c0d0c7bf7e14e305fa62cc52d98b706a959f0f
Nukie <92524294+NukieGIT@users.noreply.github.com>
2022-10-10 00:13:05 +0200

    e

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
-    <Header>
-        <ThemeSwitch />
-    </Header>
+
 </template>
 
 <script setup>
-    import ThemeSwitch from './components/ThemeSwitch.vue';
-    import Header from './components/Header.vue';
+
+    
 
 </script>
+
+<style scoped>
+
+
+
+</style>
